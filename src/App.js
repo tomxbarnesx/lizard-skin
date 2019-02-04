dc5d38fc9eb968c3c1fdd132524cf334c42d266b
Tom Barnes <barnes.thomas.c@gmail.com>
2019-02-03 21:46:38 -0500

    added 2nd layer

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 
 import cover from './images/cover.jpg';
+import head from './images/head.png';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="homeCover">
-            <img src={cover} width="100%" />
+            <img className="landscape" src={cover} width="100%" alt="The cover turned landscape"/>
+            <img className="head" src={head} width="100%" alt="A faceless lizard head" />
         </div>
+        <div></div>
       </div>
     );
   }
